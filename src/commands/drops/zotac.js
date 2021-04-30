be2673c2531de7940f85fab2155b1f98c370d6d2
Ivan Montiel <idmontie@gmail.com>
2021-04-29 18:57:13 -0700

    Fix strings

--- src/commands/drops/zotac.js
+++ src/commands/drops/zotac.js
@@ -6,7 +6,7 @@ module.exports = {
 	cooldown: 30,
 	channelID: CHANNELS.STONKS,
 	execute(message) {
-		message.channel.send(''`Zotac Steps:\n\n
+		message.channel.send(`Zotac Steps:\n\n
 · Main Page: https://www.zotacstore.com/us/graphics-cards/geforce-rtx-30-series?limit=24\n
 1. Login: https://www.zotacstore.com/us/customer/account/login/\n
 2. ADD AN ITEM TO YOUR CART
@@ -14,7 +14,7 @@ module.exports = {
 4. Start: https://store.zotac.com/paypal/express/start/\n
 5. Review: https://store.zotac.com/paypal/express/review/\n
 6. CONFIRM: POST https://store.zotac.com/paypal/express/placeOrder/
-```);
+`);
 	},
 };
 
