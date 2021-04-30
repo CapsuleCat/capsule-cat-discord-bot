a059cd038ba0836ab926c24efd453eca628d95d8
Ivan Montiel <idmontie@gmail.com>
2021-04-30 11:13:58 -0700

    Fix giphy link

--- src/commands/fun/apex.js
+++ src/commands/fun/apex.js
@@ -6,12 +6,12 @@ function pick(items) {
 const memes = [
 	'https://media.giphy.com/media/FDepi8IWqVjUKnJPMO/giphy.gif',
 	'https://media.giphy.com/media/kU264gVYaA9SEHCFXm/giphy.gif',
-	'https://media.giphy.com/media/kU264gVYaA9SEHCFXm/giphy.gif',
+	'https://media.giphy.com/media/U6RtL8iZCBz6tTFvQr/giphy.gif',
 ];
 
 module.exports = {
 	name: 'apex',
-	description: 'Its berd time',
+	description: 'Apex time',
 	cooldown: 30,
 	execute(message) {
 		message.channel.send(`${pick(memes)}`);
