95d59ec0336491b496e340236b844bfb64f93388
Ivan Montiel <idmontie@gmail.com>
2021-05-03 12:48:19 -0700

    Add another img

--- src/commands/fun/apex.js
+++ src/commands/fun/apex.js
@@ -4,9 +4,10 @@ function pick(items) {
 }
 
 const memes = [
-	'https://media.giphy.com/media/FDepi8IWqVjUKnJPMO/giphy.gif',
-	'https://media.giphy.com/media/kU264gVYaA9SEHCFXm/giphy.gif',
-	'https://media.giphy.com/media/U6RtL8iZCBz6tTFvQr/giphy.gif',
+	{ img: 'https://media.giphy.com/media/FDepi8IWqVjUKnJPMO/giphy.gif' },
+	{ img: 'https://media.giphy.com/media/kU264gVYaA9SEHCFXm/giphy.gif' },
+	{ img: 'https://media.giphy.com/media/U6RtL8iZCBz6tTFvQr/giphy.gif' },
+	{ img: 'https://tenor.com/view/apex-wraith-big-ass-gif-19294728', message: 'money_pit_rsx specifically wanted this image added' }
 ];
 
 module.exports = {
@@ -14,6 +15,8 @@ module.exports = {
 	description: 'Apex time',
 	cooldown: 30,
 	execute(message) {
-		message.channel.send(`${pick(memes)}`);
+		const meme = pick(memes);
+		const prefix = meme.message ? `${meme.message}\n\n` : '';
+		message.channel.send(`${prefix}${meme.img}`);
 	},
 };
