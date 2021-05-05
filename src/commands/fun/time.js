362bbe15088035f531d58f7c4a0128aa1af8edd5
Ivan Montiel <idmontie@gmail.com>
2021-05-05 09:02:47 -0700

    Fix time command a bit

--- src/commands/fun/time.js
+++ src/commands/fun/time.js
@@ -5,31 +5,37 @@ module.exports = {
 	description: 'Translate times',
 	args: true,
 	execute(message, args) {
-		const time = args[0];
-		const timezone = args[1] || 'CST';
-		const tx = getTimezone(timezone);
-
-		let hour = parseInt(time, 10);
-		const offsetTime = time.includes('pm');
-		if (offsetTime) {
-			hour += 12;
-		}
-
-		const now = new Date();
-
-		const dateString = `${now.getMonth() + 1}-${now.getDate()}-${now.getFullYear()} ${hour}:00:00Z${tx.utcOffsetStr}`;
-
-		const date = new Date(dateString);
-        
 		try {
-			const pt = date.toLocaleTimeString('en-US', { timeZoneName: 'short', timeZone: 'America/Los_Angeles' });
-			const ct = date.toLocaleTimeString('en-US', { timeZoneName: 'short', timeZone: 'America/Chicago' });
-			const phoenix = date.toLocaleTimeString('en-US', { timeZoneName: 'long', timeZone: 'America/Phoenix' });
+
+			const time = args[0];
+			const timezone = args[1] || 'CST';
+			const tx = getTimezone(timezone);
     
-			message.channel.send(`⏰ ${args[0]} ${args[1]} is:\n · ${pt}\n · ${phoenix}\n · ${ct}`);
+			let hour = parseInt(time, 10);
+			const offsetTime = time.includes('pm');
+			if (offsetTime) {
+				hour += 12;
+			}
+    
+			const now = new Date();
+    
+			const dateString = `${now.getMonth() + 1}-${now.getDate()}-${now.getFullYear()} ${hour}:00:00Z${tx.utcOffsetStr}`;
+    
+			const date = new Date(dateString);
+            
+			try {
+				const pt = date.toLocaleTimeString('en-US', { timeZoneName: 'short', timeZone: 'America/Los_Angeles' });
+				const ct = date.toLocaleTimeString('en-US', { timeZoneName: 'short', timeZone: 'America/Chicago' });
+				const phoenix = date.toLocaleTimeString('en-US', { timeZoneName: 'long', timeZone: 'America/Phoenix' });
+        
+				message.channel.send(`⏰ ${args[0]} ${args[1]} is:\n · ${pt}\n · ${phoenix}\n · ${ct}`);
+			} catch (e) {
+				console.error(e);
+				message.channel.send('❌ Could not parse time and timezones');
+			}
 		} catch (e) {
 			console.error(e);
-			message.channel.send('❌ Could not parse time and timezones');
+			message.channel.send('❌ Could not interpret command');
 		}
 	},
 };
