d6ad8c9c48497aa069065a1cafb1bab56903e66f
Ivan Montiel <idmontie@gmail.com>
2021-06-21 22:59:09 -0700

    Add evga command and what

--- src/commands/drops/evga.js
+++ src/commands/drops/evga.js
@@ -6,6 +6,6 @@ module.exports = {
 	cooldown: 30,
 	channelID: CHANNELS.STONKS,
 	execute(message) {
-		message.channel.send('EVGA B-Stock:\n\n https://www.evga.com/products/ProductList.aspx?type=8&family=GeForce+30+Series+Family');
+		message.channel.send(`EVGA B-Stock:\n\n https://www.evga.com/products/ProductList.aspx?type=8&family=GeForce+30+Series+Family\n\nFeel free to use our referral link: ${process.env.EVGA}\n\nOr our associate code: ${process.env.EVGA_ASSOCIATE_CODE}`);
 	},
 };
---
+++ src/commands/fun/what.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: 'allen',
+	description: 'You what',
+	cooldown: 30,
+	execute(message) {
+		message.channel.send('YOU DOWNLOAD ALL YOUR WHAT IN 4K');
+	},
+};
