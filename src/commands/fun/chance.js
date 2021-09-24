d72651405698fb034c16e5a688c164da58d77758
Ivan Montiel <idmontie@gmail.com>
2021-09-24 11:07:24 -0700

    Add chance command

---
+++ src/commands/fun/chance.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+	name: 'chance',
+	description: 'Bother Chance',
+	cooldown: 30,
+	execute(message) {
+		message.channel.send('https://www.youtube.com/watch?v=bH1Q2lyjAqY');
+	},
+};
